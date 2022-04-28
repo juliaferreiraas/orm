b7ae2357735c78b8d389c50815b0ecc125dc0c7e
julia ferreira <85883344+juliaferreiraas@users.noreply.github.com>
2022-04-28 16:47:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var { usuario } = require("./models");
+
+app.use(express.json());
+app.use(express.urlencoded({
+   extended: true
+}))
+
+app.get ("/", async function(req, res){
+  var mostrar =  await usuario.findAll();
+  res.json(mostrar);
+});
+
+app.post('/', function(req, res){
+  var resultado =  usuario.create(req.body);
+  res.json(resultado);
+});
+
+app.listen(3000, function(){
+  console.log("o servidor está bruto de mais");
+});
+
